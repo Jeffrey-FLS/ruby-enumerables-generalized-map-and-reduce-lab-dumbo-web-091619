6066f7545252ffae4f77ffa28e99c7a61799453c
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-09-13 00:52:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
 # Your Code Here
+
+def my_own_map(numbers_array1, names_array, numbers_array2) 
+  # numbers = numbers_array1.map { |num| num * -1 }
+  # yield(numbers)
+  
+  # numbers_array1.map { |num| num * -1 }
+  
+  return map(numbers_array1){ |num| num * -1 }
+  
+  # yield(names_array.map { |name| name })
+  # yield(numbers_array2.map { |num| num * 2})
+end
